92826ea3f83acddbad553e18eac564dc5d682ea1
CallumDougan <practicalitymalfunction@gmail.com>
2015-12-09 15:01:22 +0000

    added most players functionality

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,12 +4,40 @@ class PlayersController < ApplicationController
     @players = Player.all
   end
 
+  def new
+    @player = Player.new
+  end
+
   def show
     player_finder
   end
 
+  def edit
+    player_finder
+  end
+
+  def update
+    player_finder.update(player_params)
+    redirect_to(players_path)
+  end
+
+  def create
+    Player.create(player_params)
+    redirect_to(players_path)
+  end
+
+  def destroy
+    player_finder.destroy
+    redirect_to(players_path)
+  end
+
 
   private
+
+  def player_params
+    params.require(:player).permit(:name, :birthday, :broom, :image)
+  end
+
   def player_finder
     @player = Player.find(params[:id])
   end
