b90eff2583c0c9ce9ac283c3a302fd896b52e89a
CallumDougan <practicalitymalfunction@gmail.com>
2015-12-09 14:04:08 +0000

    Introduced players

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ Team.create!(name: 'Moutohora Macaws', founded: '1 March 1872', mascot: 'Sparky'
 Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barny the Fruitbat')
 Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
 
+Player.delete_all
+
+Player.create!(name: 'Barry the Rubbish', birthday: '3 January 1985', broom: 'Boring')
+Player.create!(name: 'Billy the Hopeless', birthday: '5 March 1980', broom: 'Irrelevant 4000')
+Player.create!(name: 'Some Other Boring Character', birthday: '19 August 1977', broom: 'Narrative Waste Mk 3')
+
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
